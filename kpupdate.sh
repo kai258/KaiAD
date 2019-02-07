9812f204f7ad7f653842e4b16bf5af46d65de615
KaiAD <34941733+kai258@users.noreply.github.com>
2019-02-07 21:38:21 +0800

    Update kpupdate.sh

--- kpupdate.sh
+++ kpupdate.sh
@@ -35,3 +35,24 @@ rm_cache(){
         fi
       fi  
 # }
+# user_rules(){  
+    wget --no-check-certificate -O /tmp/user-rules.txt https://raw.githubusercontent.com/kai258/KaiAD/master/user-rules.txt
+      if [ "$?"x != "0"x ]; then
+        echo_date "下载自用规则失败"
+        logger -t "【Koolproxy】" -p cron.error "下载自用规则失败"
+        rm_cache
+      else          
+        user_online=$(sed -n '1p' /tmp/user-rules.txt |  awk -F' ' '{print $3$4}'  | sed  's/-//g' | sed  's/://g')
+        user_local=$(sed -n '1p' /etc/storage/koolproxy/user-rules/user.txt |  awk -F' ' '{print $3$4}'  | sed  's/-//g' | sed  's/://g')
+        if [ "$user_online" -le "$user_local" ];then
+           echo_date "本地自用规则已经最新，无需更新"
+           logger -t "【Koolproxy】" -p cron.info "本地自用规则已经最新，无需更新"
+           rm_cache
+        else
+           echo_date "检测到自用规则更新，应用规则中..."
+           logger -t "【Koolproxy】" -p cron.info "检测到自用规则更新，应用规则中..."
+           cp -f /tmp/user-rules.txt /etc/storage/koolproxy/user-rules/user.txt
+           rm_cache;restart_kp
+        fi
+      fi  
+# }
