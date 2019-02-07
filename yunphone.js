3a54a2e241c95d2c27b44508ae4694864284f3ca
KaiAD <34941733+kai258@users.noreply.github.com>
2019-02-07 22:14:26 +0800

    Update and rename yunphone.js to dnsmasq.txt

--- yunphone.js
+++
@@ -1,8 +0,0 @@
-function youkufilm(){
-	var testingText = 'youkuVIP("brief-score","player");'
-	var rootElement = document.body;
-	var newElement = document.createElement("script");
-	var newElementHtmlContent = document.createTextNode(testingText);
-	rootElement.appendChild(newElement);
-	newElement.appendChild(newElementHtmlContent);
-}
